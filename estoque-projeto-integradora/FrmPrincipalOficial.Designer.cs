0f3c3033a4bd8bea7b46e63999ba715a4aad217e
GustavoLuizGit <glogustavo4321@gmail.com>
2021-11-18 13:15:58 -0300

    Update FrmPrincipalOficial.Designer.cs

--- estoque-projeto-integradora/FrmPrincipalOficial.Designer.cs
+++ estoque-projeto-integradora/FrmPrincipalOficial.Designer.cs
@@ -30,22 +30,23 @@ namespace estoque_projeto_integradora
         private void InitializeComponent()
         {
             this.panelMenu = new System.Windows.Forms.Panel();
-            this.panelLogo = new System.Windows.Forms.Panel();
             this.button5 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.btnCadastrar = new System.Windows.Forms.Button();
             this.btnPedidos = new System.Windows.Forms.Button();
-            this.paneltitlebar = new System.Windows.Forms.Panel();
-            this.lbltitle = new System.Windows.Forms.Label();
+            this.panelLogo = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
-            this.panelDesktop = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.paneltitlebar = new System.Windows.Forms.Panel();
             this.panelCadastrar = new System.Windows.Forms.Panel();
-            this.button7 = new System.Windows.Forms.Button();
             this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbltitle = new System.Windows.Forms.Label();
+            this.panelDesktop = new System.Windows.Forms.Panel();
+            this.button9 = new System.Windows.Forms.Button();
             this.panelMenu.SuspendLayout();
             this.panelLogo.SuspendLayout();
             this.paneltitlebar.SuspendLayout();
@@ -68,16 +69,6 @@ namespace estoque_projeto_integradora
             this.panelMenu.Name = "panelMenu";
             this.panelMenu.Size = new System.Drawing.Size(262, 689);
             this.panelMenu.TabIndex = 0;
-            // 
-            // panelLogo
-            // 
-            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(31)))), ((int)(((byte)(29)))));
-            this.panelLogo.Controls.Add(this.label1);
-            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelLogo.Location = new System.Drawing.Point(0, 0);
-            this.panelLogo.Name = "panelLogo";
-            this.panelLogo.Size = new System.Drawing.Size(262, 137);
-            this.panelLogo.TabIndex = 0;
             // 
             // button5
             // 
@@ -199,28 +190,15 @@ namespace estoque_projeto_integradora
             this.btnPedidos.UseVisualStyleBackColor = true;
             this.btnPedidos.Click += new System.EventHandler(this.btnPedidos_Click);
             // 
-            // paneltitlebar
+            // panelLogo
             // 
-            this.paneltitlebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
-            this.paneltitlebar.Controls.Add(this.panelCadastrar);
-            this.paneltitlebar.Controls.Add(this.lbltitle);
-            this.paneltitlebar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.paneltitlebar.Location = new System.Drawing.Point(262, 0);
-            this.paneltitlebar.Name = "paneltitlebar";
-            this.paneltitlebar.Size = new System.Drawing.Size(1125, 137);
-            this.paneltitlebar.TabIndex = 1;
-            // 
-            // lbltitle
-            // 
-            this.lbltitle.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.lbltitle.AutoSize = true;
-            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbltitle.ForeColor = System.Drawing.Color.White;
-            this.lbltitle.Location = new System.Drawing.Point(529, 9);
-            this.lbltitle.Name = "lbltitle";
-            this.lbltitle.Size = new System.Drawing.Size(134, 44);
-            this.lbltitle.TabIndex = 0;
-            this.lbltitle.Text = "HOME";
+            this.panelLogo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(31)))), ((int)(((byte)(29)))));
+            this.panelLogo.Controls.Add(this.label1);
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(262, 137);
+            this.panelLogo.TabIndex = 0;
             // 
             // label1
             // 
@@ -234,35 +212,17 @@ namespace estoque_projeto_integradora
             this.label1.TabIndex = 1;
             this.label1.Text = "GLM controll";
             // 
-            // panelDesktop
+            // paneltitlebar
             // 
-            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panelDesktop.Location = new System.Drawing.Point(262, 137);
-            this.panelDesktop.Name = "panelDesktop";
-            this.panelDesktop.Size = new System.Drawing.Size(1125, 552);
-            this.panelDesktop.TabIndex = 2;
-            // 
-            // button1
-            // 
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(22, 14);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(127, 49);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Funcionario";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(174, 14);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(127, 49);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "Funcionario";
-            this.button6.UseVisualStyleBackColor = true;
+            this.paneltitlebar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(150)))), ((int)(((byte)(136)))));
+            this.paneltitlebar.Controls.Add(this.button9);
+            this.paneltitlebar.Controls.Add(this.panelCadastrar);
+            this.paneltitlebar.Controls.Add(this.lbltitle);
+            this.paneltitlebar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.paneltitlebar.Location = new System.Drawing.Point(262, 0);
+            this.paneltitlebar.Name = "paneltitlebar";
+            this.paneltitlebar.Size = new System.Drawing.Size(1125, 137);
+            this.paneltitlebar.TabIndex = 1;
             // 
             // panelCadastrar
             // 
@@ -276,6 +236,17 @@ namespace estoque_projeto_integradora
             this.panelCadastrar.Size = new System.Drawing.Size(1125, 70);
             this.panelCadastrar.TabIndex = 2;
             // 
+            // button8
+            // 
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(482, 14);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(127, 49);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "Funcionario";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
             // button7
             // 
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
@@ -287,16 +258,57 @@ namespace estoque_projeto_integradora
             this.button7.Text = "Funcionario";
             this.button7.UseVisualStyleBackColor = true;
             // 
-            // button8
+            // button6
             // 
-            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(482, 14);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(127, 49);
-            this.button8.TabIndex = 2;
-            this.button8.Text = "Funcionario";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(174, 14);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 49);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Funcionario";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 49);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Funcionario";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lbltitle
+            // 
+            this.lbltitle.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbltitle.AutoSize = true;
+            this.lbltitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltitle.ForeColor = System.Drawing.Color.White;
+            this.lbltitle.Location = new System.Drawing.Point(529, 9);
+            this.lbltitle.Name = "lbltitle";
+            this.lbltitle.Size = new System.Drawing.Size(134, 44);
+            this.lbltitle.TabIndex = 0;
+            this.lbltitle.Text = "HOME";
+            // 
+            // panelDesktop
+            // 
+            this.panelDesktop.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelDesktop.Location = new System.Drawing.Point(262, 137);
+            this.panelDesktop.Name = "panelDesktop";
+            this.panelDesktop.Size = new System.Drawing.Size(1125, 552);
+            this.panelDesktop.TabIndex = 2;
+            // 
+            // button9
+            // 
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(37, 29);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 3;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
             // 
             // FrmPrincipalOficial
             // 
@@ -337,5 +349,6 @@ namespace estoque_projeto_integradora
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
     }
 }
