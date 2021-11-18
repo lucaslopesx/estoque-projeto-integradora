d25a9c74bb48eeccc0e7310b5308011f2dc0cdd5
Lucas Lopes <lucas.lopesx1@gmail.com>
2021-11-17 22:34:20 -0200

    commit a

--- estoque-projeto-integradora/frmCadastroSetor.Designer.cs
+++ estoque-projeto-integradora/frmCadastroSetor.Designer.cs
@@ -29,10 +29,17 @@ namespace estoque_projeto_integradora
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmCadastroSetor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(328, 206);
+            this.Name = "frmCadastroSetor";
             this.Text = "frmCadastroSetor";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
