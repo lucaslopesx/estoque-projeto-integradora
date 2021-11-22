0a857067f51e1bb4ecdecd8420385f0a151c56eb
GustavoLuizGit <glogustavo4321@gmail.com>
2021-11-22 02:54:10 -0300

    Cadastrar_Foto

--- estoque-projeto-integradora/Forms/Cadastrar_Produto.Designer.cs
+++ estoque-projeto-integradora/Forms/Cadastrar_Produto.Designer.cs
@@ -34,13 +34,14 @@ namespace estoque_projeto_integradora.Forms
             this.txtDesc = new System.Windows.Forms.TextBox();
             this.txtNomeProduto = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
+            this.cmdImg = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,9 +49,10 @@ namespace estoque_projeto_integradora.Forms
             // 
             this.txtPreco.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.txtPreco.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPreco.Location = new System.Drawing.Point(62, 200);
+            this.txtPreco.Location = new System.Drawing.Point(83, 246);
+            this.txtPreco.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPreco.Name = "txtPreco";
-            this.txtPreco.Size = new System.Drawing.Size(277, 35);
+            this.txtPreco.Size = new System.Drawing.Size(368, 41);
             this.txtPreco.TabIndex = 41;
             // 
             // cmdCadastrar
@@ -58,11 +60,12 @@ namespace estoque_projeto_integradora.Forms
             this.cmdCadastrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.cmdCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdCadastrar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.cmdCadastrar.Location = new System.Drawing.Point(292, 410);
-            this.cmdCadastrar.MaximumSize = new System.Drawing.Size(257, 47);
-            this.cmdCadastrar.MinimumSize = new System.Drawing.Size(257, 47);
+            this.cmdCadastrar.Location = new System.Drawing.Point(389, 505);
+            this.cmdCadastrar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdCadastrar.MaximumSize = new System.Drawing.Size(343, 58);
+            this.cmdCadastrar.MinimumSize = new System.Drawing.Size(343, 58);
             this.cmdCadastrar.Name = "cmdCadastrar";
-            this.cmdCadastrar.Size = new System.Drawing.Size(257, 47);
+            this.cmdCadastrar.Size = new System.Drawing.Size(343, 58);
             this.cmdCadastrar.TabIndex = 39;
             this.cmdCadastrar.Text = "Cadastrar Produto";
             this.cmdCadastrar.UseVisualStyleBackColor = true;
@@ -72,18 +75,20 @@ namespace estoque_projeto_integradora.Forms
             // 
             this.txtDesc.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.txtDesc.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtDesc.Location = new System.Drawing.Point(62, 127);
+            this.txtDesc.Location = new System.Drawing.Point(83, 156);
+            this.txtDesc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtDesc.Name = "txtDesc";
-            this.txtDesc.Size = new System.Drawing.Size(277, 35);
+            this.txtDesc.Size = new System.Drawing.Size(368, 41);
             this.txtDesc.TabIndex = 36;
             // 
             // txtNomeProduto
             // 
             this.txtNomeProduto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.txtNomeProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNomeProduto.Location = new System.Drawing.Point(62, 56);
+            this.txtNomeProduto.Location = new System.Drawing.Point(83, 69);
+            this.txtNomeProduto.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNomeProduto.Name = "txtNomeProduto";
-            this.txtNomeProduto.Size = new System.Drawing.Size(277, 35);
+            this.txtNomeProduto.Size = new System.Drawing.Size(368, 41);
             this.txtNomeProduto.TabIndex = 35;
             // 
             // comboBox1
@@ -91,43 +96,35 @@ namespace estoque_projeto_integradora.Forms
             this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(62, 270);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Location = new System.Drawing.Point(83, 332);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(277, 37);
+            this.comboBox1.Size = new System.Drawing.Size(368, 44);
             this.comboBox1.TabIndex = 44;
             // 
-            // pictureBox1
+            // cmdImg
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(524, 56);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(211, 179);
-            this.pictureBox1.TabIndex = 45;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button2
-            // 
-            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(563, 257);
-            this.button2.MaximumSize = new System.Drawing.Size(125, 69);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(125, 64);
-            this.button2.TabIndex = 46;
-            this.button2.Text = "Escolher imagem";
-            this.button2.UseVisualStyleBackColor = true;
+            this.cmdImg.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.cmdImg.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdImg.Location = new System.Drawing.Point(762, 360);
+            this.cmdImg.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmdImg.MaximumSize = new System.Drawing.Size(167, 85);
+            this.cmdImg.Name = "cmdImg";
+            this.cmdImg.Size = new System.Drawing.Size(167, 79);
+            this.cmdImg.TabIndex = 46;
+            this.cmdImg.Text = "Escolher imagem";
+            this.cmdImg.UseVisualStyleBackColor = true;
+            this.cmdImg.Click += new System.EventHandler(this.cmdImg_Click);
             // 
             // label1
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(57, 24);
+            this.label1.Location = new System.Drawing.Point(76, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 29);
+            this.label1.Size = new System.Drawing.Size(246, 36);
             this.label1.TabIndex = 47;
             this.label1.Text = "Nome do produto";
             // 
@@ -136,9 +133,10 @@ namespace estoque_projeto_integradora.Forms
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(57, 99);
+            this.label2.Location = new System.Drawing.Point(76, 122);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(121, 29);
+            this.label2.Size = new System.Drawing.Size(147, 36);
             this.label2.TabIndex = 48;
             this.label2.Text = "Descrição";
             // 
@@ -147,9 +145,10 @@ namespace estoque_projeto_integradora.Forms
             this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(57, 171);
+            this.label3.Location = new System.Drawing.Point(76, 210);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 29);
+            this.label3.Size = new System.Drawing.Size(93, 36);
             this.label3.TabIndex = 49;
             this.label3.Text = "Preço";
             // 
@@ -158,9 +157,10 @@ namespace estoque_projeto_integradora.Forms
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(57, 243);
+            this.label4.Location = new System.Drawing.Point(76, 299);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(71, 29);
+            this.label4.Size = new System.Drawing.Size(86, 36);
             this.label4.TabIndex = 50;
             this.label4.Text = "Setor";
             // 
@@ -169,19 +169,37 @@ namespace estoque_projeto_integradora.Forms
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(519, 18);
+            this.label5.Location = new System.Drawing.Point(692, 22);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(223, 29);
+            this.label5.Size = new System.Drawing.Size(271, 36);
             this.label5.TabIndex = 51;
             this.label5.Text = "Imagem do produto";
             // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::estoque_projeto_integradora.Properties.Resources.semfoto;
+            this.pictureBox1.Location = new System.Drawing.Point(699, 69);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(281, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 45;
+            this.pictureBox1.TabStop = false;
+            // 
             // Cadastrar_Produto
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(791, 553);
+            this.ClientSize = new System.Drawing.Size(1055, 681);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cmdImg);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.txtPreco);
@@ -192,7 +210,7 @@ namespace estoque_projeto_integradora.Forms
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Cadastrar_Produto";
             this.Text = "Cadastrar_Produto";
             this.Load += new System.EventHandler(this.Cadastrar_Produto_Load);
@@ -210,11 +228,12 @@ namespace estoque_projeto_integradora.Forms
         private System.Windows.Forms.TextBox txtNomeProduto;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button cmdImg;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
