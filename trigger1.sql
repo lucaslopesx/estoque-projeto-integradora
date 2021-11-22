244ec1e32251245d418531b2bf77b8c0eec0c692
GustavoLuizGit <glogustavo4321@gmail.com>
2021-11-22 02:55:09 -0300

    Update trigger1.sql

--- trigger1.sql
+++ trigger1.sql
@@ -4,16 +4,16 @@ after insert
 as
 begin
 
-	declare @idPedInserted int
-	declare @qntdPedInserted int
-	declare @precoPedInserted money
+    declare @idPedInserted int
+    declare @qntdPedInserted int
+    declare @precoPedInserted money
 
-	set @idPedInserted = (select idPedido from inserted)
-	set @precoPedInserted = (select precoItensPedido from inserted)
-	set @qntdPedInserted = (select quantidadeItensPedido from inserted)
+    set @idPedInserted = (select idPedido from inserted)
+    set @precoPedInserted = (select precoItensPedido from inserted)
+    set @qntdPedInserted = (select quantidadeItensPedido from inserted)
 
-	update Pedidos 
-	set preco = preco + @precoPedInserted
-	where idPedido = @idPedInserted
-	
+    update Pedidos 
+    set preco = preco + (@precoPedInserted * @qntdPedInserted) 
+    where idPedido = @idPedInserted
+
 end
